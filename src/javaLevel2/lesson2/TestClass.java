85e568345dda72c71d3fc4f9929e1b9a8655220a
manunia <timoshenkomaria@mail.ru>
2020-02-03 17:21:09 +0300

    В методе main() вызваем метод, обрабатываем возможные исключения MySizeArrayException и MyArrayDataException и выводим результат

--- src/javaLevel2/lesson2/TestClass.java
+++ src/javaLevel2/lesson2/TestClass.java
@@ -2,8 +2,25 @@ package javaLevel2.lesson2;
 
 public class TestClass {
 
+    public static void main(String[] args) {
+        String[][] strings = {
+                {"1","2","3","4"},
+                {"1","2","3","4"},
+                {"1","2","3","4"},
+                {"1","+","3","4"}
+        };
+        try {
+            myArray(strings);
+        } catch (MyArraySizeException e) {
+            System.out.println(e.toString());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.toString());
+        }
 
-    private void myArray(String[][] srtArr) throws MyArraySizeException, MyArrayDataException {
+    }
+
+
+    private static void myArray(String[][] srtArr) throws MyArraySizeException, MyArrayDataException {
 
         if (srtArr.length != 4 || srtArr[0].length != 4) throw new MyArraySizeException();
         int sum = 0;
@@ -16,6 +33,7 @@ public class TestClass {
                 }
             }
         }
+        System.out.println(sum);
 
     }
 }
