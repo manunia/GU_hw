abd4117e8c31bde719909d6f920d549f471f2ccc
manunia <timoshenkomaria@mail.ru>
2020-02-03 17:14:03 +0300

    создали класс исключение  MyArrayDataException, переопределили в нем метод toString() и сделали специальный конструктор, чтобы выводить детализацию по ячейке; в методе проходимся по массиву, преобразуем элементы в число и суммируем. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), брошено исключение MyArrayDataException

--- src/javaLevel2/lesson2/TestClass.java
+++ src/javaLevel2/lesson2/TestClass.java
@@ -3,9 +3,19 @@ package javaLevel2.lesson2;
 public class TestClass {
 
 
-    private void myArray(String[][] srtArr) throws MyArraySizeException {
+    private void myArray(String[][] srtArr) throws MyArraySizeException, MyArrayDataException {
 
-        if (srtArr.length != 4) throw new MyArraySizeException();
+        if (srtArr.length != 4 || srtArr[0].length != 4) throw new MyArraySizeException();
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (srtArr[i][j].matches("^-?\\d+$")) {
+                    sum += Integer.parseInt(srtArr[i][j]);
+                } else {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
 
     }
 }
